18d2fbf669a47e9719d62d4df464b4390e697582
Marco Pennekamp <marco.pennekamp96@gmail.com>
2017-12-08 15:57:35 +0100

    Update similarity and path methods. Fix root name.

--- metonym/__init__.py
+++ metonym/__init__.py
@@ -3,6 +3,7 @@ import math
 import networkx as nx
 
 MAX_LENGTH = 100
+SYNSET_LEMMA_SEPARATOR = '//'
 
 
 class Node:
@@ -11,11 +12,11 @@ class Node:
         self.lemma = lemma
 
     def key(self):
-        return f'{self.synset.name()}//{self.lemma.name()}'
+        return f'{self.synset.name()}{SYNSET_LEMMA_SEPARATOR}{self.lemma.name()}'
 
     @staticmethod
     def from_key(key):
-        parts = key.split('//')
+        parts = key.split(SYNSET_LEMMA_SEPARATOR)
         synset = wn.synset(parts[0])
         return Node.from_synset_and_lemma_name(synset, parts[1])
 
@@ -32,27 +33,27 @@ class WordNetGraph(object):
 
         # The name of the graph's "root" node, in reference to the taxonomy induced
         # by the hypernymy relation.
-        self.root_name = 'entity.n.01'
+        self.root_name = f'entity.n.01{SYNSET_LEMMA_SEPARATOR}entity'
 
         # The taxonomy depth is the maximum distance from the synset root (entity.n.01) to
         # any other synset that can be reached via any edges.
         lengths = list(nx.shortest_path_length(self.graph, self.root_name, weight='weight').values())
         self.taxonomy_depth = max(lengths)
 
-    def shortest_path(self, synset1, synset2):
+    def shortest_path(self, node1, node2):
         try:
-            return nx.shortest_path(self.graph, synset1.name(), synset2.name(), weight='weight')
+            return nx.shortest_path(self.graph, node1.key(), node2.key(), weight='weight')
         except nx.NetworkXNoPath:
             return None
 
-    def shortest_path_distance(self, synset1, synset2):
+    def shortest_path_distance(self, node1, node2):
         try:
-            return nx.shortest_path_length(self.graph, synset1.name(), synset2.name(), weight='weight')
+            return nx.shortest_path_length(self.graph, node1.key(), node2.key(), weight='weight')
         except nx.NetworkXNoPath:
             return None
 
-    def lch_similarity(self, synset1, synset2):
-        distance = self.shortest_path_distance(synset1, synset2)
+    def lch_similarity(self, node1, node2):
+        distance = self.shortest_path_distance(node1, node2)
         if distance is None:
             distance = self.taxonomy_depth
 
